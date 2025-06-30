c996c65914331971b8e04d036918352533812f84
WR624KQI <0000877673@senaimgaluno.com.br>
2025-06-30 18:53:35 -0300

    feat: pag inicial

---
+++ src/app/contatos/page.js
@@ -0,0 +1,10 @@
+export default function Contatos() { 
+    return (
+        <div className ="flex">
+            <h1>Bem-vindoa Calculadora RPV</h1>
+            <h2>Projeto base Next.Js + Tailwind + shadcn + Integração Backend</h2>
+            <p>Esse projeto foi criado para demonstrar uma calculadora de operações matematica integrando frontend moderno com backend externo, utilizando as melhores praticas deo ecossistema Reac/Next.JS</p>
+            <button></button>
+        </div>
+    )                     
+}
